da2d28e9b6d339606b14fb0ca8eceeab63869ef7
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-30 17:08:47 +0700

    Create class and method for node

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace StackUsingLinkedList_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
