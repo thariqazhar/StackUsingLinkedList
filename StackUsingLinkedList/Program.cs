1377e7c6d65932d2691a787a4a1b7aa50e633bb7
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-30 17:27:19 +0700

    Create main menu

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -61,5 +61,46 @@ namespace StackUsingLinkedList_CSharp
                 Console.WriteLine();
             }
         }
+
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
+        }
     }
 }
