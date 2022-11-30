601be31b30c3e655e205f4d85abc01c4b61054de
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-30 17:09:45 +0700

    Create class and method for stacks

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -12,4 +12,14 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
+        }
+    }
 }
