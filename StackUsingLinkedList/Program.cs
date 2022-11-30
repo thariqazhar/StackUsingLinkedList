fc443e4a7b3814740c5109dc6311cf721e4ecf2c
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-30 17:17:22 +0700

    Create a method for display

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -45,5 +45,21 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next; //Make top point to the next node in sequence
         }
+
+        public void display()
+        {
+            Node tmp;
+            if (empty()) //If stack is empty
+                Console.WriteLine("\nStack is empty");
+            else
+            {
+                //Traverse the list from the beginning till the end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
